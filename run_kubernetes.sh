b0601e1ef578ef148db01bd9e97efee1afebf437
antonnifo <antonnifo@live.com>
2023-03-10 13:11:31 +0300

    tags and uploads an image to Docker Hub

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=antonnifo/flaskmlproject
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run flaskmlproject-api --image=$dockerpath:latest
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward flaskmlproject-api 8000:80
 
