e6d2d71f31f5696c403da6d56570085195d5f978
antonnifo <antonnifo@live.com>
2023-03-10 13:12:27 +0300

    tag& upload an image to Docker Hub

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=antonnifo/flaskmlproject
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u antonnifo
+docker tag flaskmlproject $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push antonnifo/flaskmlproject
